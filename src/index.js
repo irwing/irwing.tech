3774c144a08d6fd18d5daeff77caf21440a651a4
Irwing Naranjo <khalisser@gmail.com>
2022-04-02 14:20:16 -0300

    🔧 Config tests and 🐛 bug react 18 dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(
-  document.getElementById('root')
-).render(
-    <React.StrictMode>
+import App from 'App'
+import 'index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
